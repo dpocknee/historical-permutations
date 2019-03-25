9ee2a28758b6ef8c5b30cce3b82ee6a4816eabf4
dpocknee <dappy_iodo@hotmail.com>
2019-03-25 10:51:08 +0000

    made sure the test files point to the index not the algorithm folder.

--- spec/myrvold-ruskey.spec.js
+++ spec/myrvold-ruskey.spec.js
@@ -1,66 +1,78 @@
-const { expect } = require('chai');
-const { uniq, uniqWith, isEqual } = require('lodash');
+const { expect } = require("chai");
+const { uniq, uniqWith, isEqual } = require("lodash");
 
-const myrvoldRuskey = require('../algorithms/2001_myrvold-ruskey');
+const { myrvoldRuskey } = require("../index");
 
-describe('Myrvold-Ruskey (2001)', () => {
-  describe('5 Elements', () => {
+describe("Myrvold-Ruskey (2001)", () => {
+  describe("5 Elements", () => {
     const startArray = [1, 2, 3, 4, 5];
     const testArrays = myrvoldRuskey(5);
     // console.log(testArrays);
-    it('checks an array of 5 elements outputs 120 permutations', () => {
+    it("checks an array of 5 elements outputs 120 permutations", () => {
       expect(testArrays.length).to.equal(120);
     });
-    it('checks all 120 permutations are unique', () => {
+    it("checks all 120 permutations are unique", () => {
       const uniqueTest = uniqWith(testArrays, isEqual).length;
       expect(uniqueTest).to.equal(120);
     });
-    it('checks all permutations only contain one of every element', () => {
-      const singularElements = testArrays.every(array => uniq(array).length === array.length);
+    it("checks all permutations only contain one of every element", () => {
+      const singularElements = testArrays.every(
+        array => uniq(array).length === array.length
+      );
       expect(singularElements).to.equal(true);
     });
-    it('checks each permutation only uses valid elements provided', () => {
-      const validElements = testArrays.every(array => array.every(element => startArray.includes(element)));
+    it("checks each permutation only uses valid elements provided", () => {
+      const validElements = testArrays.every(array =>
+        array.every(element => startArray.includes(element))
+      );
       expect(validElements).to.equal(true);
     });
   });
-  describe('4 Elements', () => {
+  describe("4 Elements", () => {
     const startArray = [1, 2, 3, 4];
     const testArrays = myrvoldRuskey(4);
     // console.log(testArrays);
-    it('checks an array of 4 elements outputs 24 permutations', () => {
+    it("checks an array of 4 elements outputs 24 permutations", () => {
       expect(testArrays.length).to.equal(24);
     });
-    it('checks all 24 permutations are unique', () => {
+    it("checks all 24 permutations are unique", () => {
       const uniqueTest = uniqWith(testArrays, isEqual).length;
       expect(uniqueTest).to.equal(24);
     });
-    it('checks all permutations only contain one of every element', () => {
-      const singularElements = testArrays.every(array => uniq(array).length === array.length);
+    it("checks all permutations only contain one of every element", () => {
+      const singularElements = testArrays.every(
+        array => uniq(array).length === array.length
+      );
       expect(singularElements).to.equal(true);
     });
-    it('checks each permutation only uses valid elements provided', () => {
-      const validElements = testArrays.every(array => array.every(element => startArray.includes(element)));
+    it("checks each permutation only uses valid elements provided", () => {
+      const validElements = testArrays.every(array =>
+        array.every(element => startArray.includes(element))
+      );
       expect(validElements).to.equal(true);
     });
   });
-  describe('3 Elements', () => {
+  describe("3 Elements", () => {
     const startArray = [1, 2, 3];
     const testArrays = myrvoldRuskey(3);
     // console.log(testArrays);
-    it('checks an array of 3 elements outputs 6 permutations', () => {
+    it("checks an array of 3 elements outputs 6 permutations", () => {
       expect(testArrays.length).to.equal(6);
     });
-    it('checks all 6 permutations are unique', () => {
+    it("checks all 6 permutations are unique", () => {
       const uniqueTest = uniqWith(testArrays, isEqual).length;
       expect(uniqueTest).to.equal(6);
     });
-    it('checks all permutations only contain one of every element', () => {
-      const singularElements = testArrays.every(array => uniq(array).length === array.length);
+    it("checks all permutations only contain one of every element", () => {
+      const singularElements = testArrays.every(
+        array => uniq(array).length === array.length
+      );
       expect(singularElements).to.equal(true);
     });
-    it('checks each permutation only uses valid elements provided', () => {
-      const validElements = testArrays.every(array => array.every(element => startArray.includes(element)));
+    it("checks each permutation only uses valid elements provided", () => {
+      const validElements = testArrays.every(array =>
+        array.every(element => startArray.includes(element))
+      );
       expect(validElements).to.equal(true);
     });
   });
